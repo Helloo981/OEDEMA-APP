9fe9bfc76d40b7b5c8ba415717dd9a1339e0f570
JoaoRibeirox <102150982+JoaoRibeirox@users.noreply.github.com>
2023-05-09 14:34:58 +0100

    Exercises Page

---
+++ oedema-app-v1/Assets/Scripts/OpenLink3.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink3 : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+
+        Application.OpenURL("https://www.youtube.com/watch?v=6_bhKLU4qcA");
+    }
+
+
+}
