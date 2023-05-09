3eeac16d85c2d02df42ae6428e8f255563cea423
Ed2003 <101203411+Ed2003@users.noreply.github.com>
2023-05-09 19:25:13 +0100

    General Advice Links

---
+++ oedema-app-v1/Assets/Scripts/CellulitisAdviceLink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellulitisAdviceLink : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://www.nhs.uk/conditions/cellulitis/");
+
+    }
+}
---
+++ oedema-app-v1/Assets/Scripts/SkincareAdviceLink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkincareAdviceLink : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://www.cancerresearchuk.org/about-cancer/coping/physically/lymphoedema-and-cancer/treating/caring-for-your-skin");
+
+    }
+}
