9cb33ab4977fe94f0a0421711bf5ce4b17d1f4b7
Husam Iqbal <hus4m321@gmail.com>
2023-05-10 16:31:06 +0100

    .

---
+++ oedema-app-v1/Assets/Scripts/OpenLymphink2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLymphink2 : MonoBehaviour
+{
+
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=UebHyphaVTM")
+    }
+}
